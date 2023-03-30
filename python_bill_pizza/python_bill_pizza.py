fb9138f93d07d5b1811f5c3c91c3c8a5163eadd6
jamker19 <l1517705@std.yildiz.edu.tr>
2023-03-30 10:46:36 +0300

    hello

---
+++ python_bill_pizza/python_bill_pizza.py
@@ -0,0 +1,26 @@
+﻿# 🚨 Don't change the code below 👇
+print("Welcome to Python Pizza Deliveries!")
+size = input("What size pizza do you want? S, M, or L ")
+add_pepperoni = input("Do you want pepperoni? Y or N ")
+extra_cheese = input("Do you want extra cheese? Y or N ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+bill=0
+ 
+if size=='S':
+    if add_pepperoni == 'Y':
+        bill = 17
+    
+elif size == 'M':
+    if add_pepperoni =='Y':
+        bill=23
+elif size == 'L':
+    if add_pepperoni=='Y':
+        bill=28
+if extra_cheese == 'Y':
+    bill=bill+1
+
+print(bill)
+
+        
